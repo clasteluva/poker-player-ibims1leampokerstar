4292e0879008422c29e5e02b1786fb433ae60e03
Claudio Iacovozzi <claudio.iacovozzi@tyrion.local>
2017-12-14 10:02:33 +0100

    klasdjf

--- Player.js
+++ Player.js
@@ -1,12 +1,17 @@
 class Player {
   static get VERSION() {
-    return '0.22';
+    return '0.23';
   }
 
   static betRequest(gameState, bet) {
     console.log("gamestate: " + JSON.stringify(gameState));
 
-    bet(10);
+    var inAction = gameState["in_action"];
+    var callValue = gameState["current_buy_in"] - gameState["players"][inAction]["bet"];
+    console.log("inAction :" + inAction);
+    console.log("callValue : " + callValue);
+
+    bet(callValue);
   }
 
   static showdown(gameState) {
